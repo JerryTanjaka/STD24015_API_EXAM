4204da1ac8da2f0d432e3139f9011193b42ca4d8
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-07-31 08:56:18 +0300

    add : add post posts request

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
+from datetime import datetime
 from fastapi import FastAPI, Request
 from starlette.responses import JSONResponse, Response
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+class Post(BaseModel):
+    author: str
+    title: str
+    content :str
+    creation:datetime
 
-
-class User(BaseModel):
-    name: str
-    age: int
-
-
-@app.get("/")
-def root():
-    return JSONResponse(content={"message": "Hello, World!"}, status_code=200)
-
-
-@app.post("/user")
-def create_user(user: User, request: Request):
-    accept_header = request.headers.get("Accept")
-    if accept_header != "text/plain":
-        return JSONResponse(content={"message": "Unsupported Media Type"}, status_code=400)
-    return JSONResponse(content={"User": user.model_dump()}, status_code=200)
+#@app.post("/user")
+#def create_user(user: User, request: Request):
+#    accept_header = request.headers.get("Accept")
+#    if accept_header != "text/plain":
+#        return JSONResponse(content={"message": "Unsupported Media Type"}, status_code=400)
+#    return JSONResponse(content={"User": user.model_dump()}, status_code=200)
 
 
 @app.get("/user")
@@ -45,3 +41,9 @@ def catch_all(full_path: str):
     with open("notFound.html", "r", encoding="utf-8") as file:
         html_content = file.read()
     return Response(content=html_content, status_code=404, media_type="text/html")
+
+post_memory: List[Post] = []
+@app.post("/posts", status_code=201)
+def create_players(list_posts: List[Post]):
+    post_memory.extend(list_posts)
+    return post_memory
